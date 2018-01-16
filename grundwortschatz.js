20bb7a103be43e097de23037b51770c8f332144c
Erik <ashburnere@gmail.com>
2018-01-16 21:10:18 +0100

    Update grundwortschatz.js

--- grundwortschatz.js
+++ grundwortschatz.js
@@ -1,15 +1,25 @@
-function init() {
-alert(data.words.length);
-  showOtherWord();
-}
+var lastIndex=-1;
 
-
-
+// data is the JSON object from file grundwortschatz.java  
+var wordCount=data.words.length;
 
 function showOtherWord() {
-    const div = document.getElementById('words');
-  div.innerHTML=words.words[0];
+  
+  const div = document.getElementById('words');
+  var random = getRandomInt(0, wordCount);
+  while (random == lastIndex) {
+    random = getRandomInt(0, wordCount);
+  };
+  
+  lastIndex=random;
+    
+  div.innerHTML=data.words[lastIndex];
 }
-
+// gets a random int between min (inclusive) and max (exclusive)
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max); 
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
 
