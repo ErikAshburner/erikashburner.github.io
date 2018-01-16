4a199edc15201f2fb30024acb00e18d39e1500dd
Erik <ashburnere@gmail.com>
2018-01-16 20:40:57 +0100

    Update 100woerter.js

--- 100woerter.js
+++ 100woerter.js
@@ -1,1 +1,16 @@
+function init() {
+  var words = JSON.parse(100woerter);
+alert(words.words.length);
+  showOtherWord();
+}
+
+
+
+
+function showOtherWord() {
+    const div = document.getElementById('words');
+  div.innerHTML=words.words[0];
+}
+
+
 
