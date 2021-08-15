e6f6b247934a06645f0c441665818369f03e7aa6
Erik Aschenbrenner <ashburnere@gmail.com>
2021-08-15 18:19:31 +0200

    Add search

--- map_filter.js
+++ map_filter.js
@@ -1,21 +1,46 @@
- function fetchAuthors() {
+const url = 'https://randomuser.me/api/?results=1000';
+var personList = [];
+
+ // fetch the data and add a list entry for each
+ function fetchPersons() {
     const personListTextArea = document.getElementById('personList');
+    const searchListTextArea = document.getElementById('searchList');
+    const foundListTextArea = document.getElementById('foundList');
     // remove existing list entries
     personListTextArea.value ='';
-    // fetch the data and add a list entry for each
-    const url = 'https://randomuser.me/api/?results=1000';
-    const list = [];
+    searchList.value ='';
+    foundList.value ='';
+    list = [];
     fetch(url)
     .then((resp) => resp.json())
     .then(function(data) {
-      let authors = data.results;
-      return authors.map(function(author) {
-        var name = author.name.first + " " + author.name.last;
-        list.push(name);
+      let persons = data.results;
+      return persons.map(function(author) {
+        var name = person.name.first + " " + person.name.last;
+        personList.push(name);
         personListTextArea.value +=  name + '\r\n';
       })
     })
     .catch(function(error) {
       console.log(JSON.stringify(error));
     });   
+
+    // search persons in the person list and add them to the found list
+    function searchPersons() {
+      const searchListTextArea = document.getElementById('searchList');
+      const foundListTextArea = document.getElementById('foundList');
+
+      var searchList = searchListTextArea.val().split('\n');
+      console.log(searchList);
+      var foundList = personList.filter(function (person) {
+        return searchList.some(name => name.toLowerCase() == person.toLowerCase());
+      });
+
+      foundList.forEach(person => {
+        var name = person.name.first + " " + person.name.last;
+        foundListTextArea.value +=  name + '\r\n';
+
+      };)
+
+
   }
